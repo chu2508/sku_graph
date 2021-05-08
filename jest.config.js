66d4fdd10cced7f087163fa6d756563e2d6000d1
dirac <767848898@qq.com>
2021-05-08 15:28:56 +0800

    init

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-commonjs */
+module.exports = {
+  "transform": {
+    "^.+\\.(ts|tsx)$": "typescript-babel-jest"
+  },
+  "moduleFileExtensions": ["ts", "tsx", "js"],
+  "testRegex": "src/*.*\\.test\\.(ts|tsx)$"
+};
